58e46507b19d79d29d4b9fb9f67392f0e659cbef
harixh <kofworld85@gmail.com>
2024-04-19 23:01:08 +0530

    added new ui for search as well as general information for admin

--- src/main.py
+++ src/main.py
@@ -1,21 +1,247 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+from scgraph.geographs.marnet import marnet_geograph
 import folium
+import geopandas as gpd
+import json
+from shapely.geometry import Point
 
 app = Flask(__name__)
 
+f = open('ports.json')
+data = json.load(f)
+ports_gdf = gpd.GeoDataFrame(data)
+ports_gdf['geometry'] = ports_gdf.apply(lambda row: Point(row['Longitude'], row['Latitude']), axis=1)
+
+
+def geocode_location(location):
+    url = f"https://lorrycare.com:5000/v1/suggest-name?input={location}"
+    response = requests.get(url)
+    data = response.json()
+    data = data[0]
+
+    print(data)
+    print(data["description"])
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={data["description"]}&key=AIzaSyDluUHxWIlx9s8aaSHIUGp2YCynHftXcPY"
+    response = requests.get(url)
+    data = response.json()
+    latlongs = data["results"][0]["geometry"]["location"]
+    print(latlongs)
+    return latlongs["lat"], latlongs["lng"]
+
 @app.route('/')
 def index():
-    # Create a map centered around a specific location
-    m= folium.Map(location=[51.5074, -0.1278], zoom_start=10)
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['user_input']
+    print("User input:", user_input)
+
+    user_input1 = request.form['user_input1']
+    print("User input:", user_input1)
+
+    user_date = request.form['user_date']
+    print("User input:", user_date)
+
+
+    location = user_input
+    latitude, longitude = geocode_location(location)
+    if latitude and longitude:
+      origin = (f"{longitude},{latitude}")
+
+      #Destination 
+
+    location1 = user_input1
+    latitude, longitude = geocode_location(location1)
+    if latitude and longitude:
+      destination = (f"{latitude},{longitude}")
+      end_latitude =  latitude
+      end_longitude = longitude
+
+
+   
+    start_coordinates = origin # probleam 
+
+
+    print(origin)
+    your_point = gpd.GeoDataFrame({'geometry': [Point(start_coordinates.split(',')[0], start_coordinates.split(',')[1])]}, crs=ports_gdf.crs)
+    nearest_port_index = ports_gdf.distance(your_point.unary_union)
+    sorted = nearest_port_index.sort_values()
+    indexes = sorted.iloc[:3].index
+    print(indexes)
+    import requests
+    import folium
+    import polyline
+    from pyproj import CRS, Transformer
+    crs_hk80 = CRS.from_proj4("+proj=tmerc +lat_0=22.31213333333334 +lon_0=114.1785555555556 +k=1 +x_0=836694.05 +y_0=819069.8 +ellps=intl +towgs84=-162.619,-276.959,-161.764,0.067753,-2.24365,-1.15883,-1.09425 +units=m +no_defs ")
+    transformer = Transformer.from_crs(crs_hk80, 4326)
+    transformer.transform(835000, 812000)
+    INTERSECTION_ID = 'Start'
+    url = "https://api.openrouteservice.org/v2/directions/driving-car/json"
+    # api_key = "5b3ce3597851110001cf6248846d6a68ec334aaaa34195cdcd3f11ad"
+    api_key = "5b3ce3597851110001cf6248bef52906ad594343afbb98f64d4f4fa0"
+    failed_coords = []
+    # end_coordinates = "72.963540, 18.952151"
+    m = folium.Map(location=(start_coordinates.split(',')[1], start_coordinates.split(',')[0]))
+    folium.Marker((start_coordinates.split(',')[1], start_coordinates.split(',')[0]), tooltip=folium.Tooltip(INTERSECTION_ID, permanent=True)).add_to(m)
+
+
+    ports = []
+    for i in indexes:
+        ports.append((ports_gdf.iloc[i]["Latitude"],ports_gdf.iloc[i]["Longitude"]))
+        print(ports_gdf.iloc[i]["Latitude"],ports_gdf.iloc[i]["Longitude"])
+        end_coordinates = str(ports_gdf.iloc[i]["Longitude"]) + "," + str(ports_gdf.iloc[i]["Latitude"])
+        body = {
+            "coordinates": [start_coordinates.split(','), end_coordinates.split(',')],
+            "radiuses": [-1]
+        }
+        print(body)
+        headers = {
+            "Authorization": api_key
+        }
+
+        response = requests.post(url, json=body, headers=headers)
+        COORDINATES_LIST = []
+        
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            COORDINATES_LIST = data["routes"][0]["geometry"]
+            decoded_coordinates = polyline.decode(COORDINATES_LIST)
+            COORDINATES_LIST= []
+            for lat, lon in decoded_coordinates:
+                COORDINATES_LIST.append([lat, lon])
+            folium.PolyLine(COORDINATES_LIST, color='red').add_to(m)
+        else:
+            failed_coords.append(end_coordinates)
+            print("Failed to fetch directions. Status code:", response.json())
+
+
+    print(ports)
+    o = ports[0][0]
+    d = ports[0][1]
+    eo = end_latitude
+    ed = end_longitude
+
+
+    ox = ports[1][0]
+    dx = ports[1][1]
+    eox = end_latitude
+    edx = end_longitude
+
+
+    oy = ports[2][0]
+    dy = ports[2][1]
+    eoy = end_latitude
+    edy = end_longitude
+
+
+    #ad marker
+
+    folium.Marker(
+        location=[o,d],
+        tooltip="Click me!",
+        popup="Sea Port",
+        icon=folium.Icon(color='black'),
+    ).add_to(m)
+
+
+
+    output = marnet_geograph.get_shortest_path(
+        origin_node={"latitude":o,"longitude":d},
+        destination_node={"latitude": eo,"longitude": ed},
+        output_units='km'
+    )
+
+    output1 = marnet_geograph.get_shortest_path(
+        origin_node={"latitude": ox,"longitude":dx },
+        destination_node={"latitude":eox,"longitude": edx},
+        output_units='km'
+    )
+
+    output2 = marnet_geograph.get_shortest_path(
+        origin_node={"latitude": oy, "longitude":dy},
+        destination_node={"latitude":eoy,"longitude":edy},
+        output_units='km'
+    )
+
+
+
+    x = (([[i['latitude'],i['longitude']] for i in output['coordinate_path']]))
+    y = (([[i['latitude'],i['longitude']] for i in output1['coordinate_path']]))
+    z = (([[i['latitude'],i['longitude']] for i in output2['coordinate_path']]))
+
+    # Show the length
+    lengthx = int(output['length']) #=> Length:  19596.4653
+    lengthy = int(output1['length']) #=> Length:  19596.4653
+    lengthz = int(output2['length']) #=> Length:  19596.4653
+
+    print("Km",lengthx,"Time",lengthx/37)
+    print("Km",lengthy,"Time",lengthy/37)
+    print("Km",lengthz,"Time",lengthz/37)
+
+    find_shortest_route = [lengthx,lengthy,lengthz]
+
+    shortest_route = (min(find_shortest_route))
+    print(shortest_route)
+
+
+    line_colorx = "blue"
+    line_colory ="blue"
+    line_colorz = "blue"
+
+    for i in range(len(find_shortest_route)):
+     if find_shortest_route[i] == shortest_route:
+        if i == 0:
+         print("Green x")
+         line_colorx= "green"
+
+        if i == 1:
+         print("Green y")
+         line_colorx= "green"
+        
+
+        if i == 2:
+         print("Green z")
+         line_colorx= "green"
+
+
     folium.TileLayer('cartodbdark_matter').add_to(m)
+    folium.PolyLine(x, color=line_colorx, weight=2.5, opacity=1 ,tooltip=('KM :',lengthx)).add_to(m)
+    folium.PolyLine(y, color=line_colory, weight=2.5, opacity=1,tooltip=('KM',lengthy)).add_to(m)
+    folium.PolyLine(z, color=line_colorz, weight=2.5, opacity=1,tooltip=('KM',lengthz)).add_to(m)
 
-    # Add a marker to the map
-    folium.Marker([51.5074, -0.1278], popup='London').add_to(m)
+    #
 
-    # Render the map in the HTML template
-    map_html = m._repr_html_()
+    m.save("templates/map.html")
+    print("New Map is return")
 
-    return render_template('index.html', map_html=map_html)
+        
+
+
+    return render_template('main.html')
+
+
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html')  # Serve the map.html file
+
+@app.route('/cost')
+def cost():
+    return render_template('cost.html')  # Serve the map.html file
+
+@app.route('/delivery')
+def delivery():
+    return render_template('delivery.html')  # Serve the map.html file
+
+@app.route('/sugggested')
+def suggested():
+    return render_template('suggested.html')  # Serve the map.html file
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
